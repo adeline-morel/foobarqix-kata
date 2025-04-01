ede12c7b8859afb7fe404a6ed00fa756650c45d1
Adeline MOREL <adeline-externe.morel@edf.fr>
2025-04-01 12:01:57 +0000

    foobarqix kata

--- src/foobarqix.ts
+++ src/foobarqix.ts
@@ -1,11 +1,75 @@
 export class FooBarQix {
-  compute(entry: number): string {
-    if(entry === 0) {
-      return '0';
+  private readonly FOO: string = 'Foo';
+  private readonly BAR: string = 'Bar';
+  private readonly QIX: string = 'Qix';
+  private readonly ASTERISK = '*';
+
+  private readonly entry: number;
+
+  constructor(entry: number) {
+    this.entry = entry;
+  }
+
+  compute() {
+    if (this.entry === 0) {
+      return this.ASTERISK;
     }
-    if(entry %3 === 0) {
-      return 'Foo';
+
+    if (this.containsDigitZero() && this.isNotFooBarQixMultiple()) {
+      return this.entry.toString().replace(/0/g, this.ASTERISK);
     }
-    return entry.toString();
+
+    if (this.isFooBarQixNumber()) {
+      return this.computeMultiple() + this.computeDigits();
+    }
+
+    return this.entry.toString();
+  }
+
+  private computeMultiple() {
+    let result = [];
+
+    if (this.isMultipleOfThree()) result.push(this.FOO);
+    if (this.isMultipleOfFive()) result.push(this.BAR);
+    if (this.isMultipleOfSeven()) result.push(this.QIX);
+
+    return result.join('');
+  }
+
+  private computeDigits() {
+    const replacements: { [key: string]: string } = { '3': this.FOO, '5': this.BAR, '7': this.QIX, '0': this.ASTERISK };
+
+    return this.entry.toString().split('')
+      .map(digit => replacements[digit] || '')
+      .join('');
+  }
+
+  private isFooBarQixNumber() {
+    return this.isMultipleOfThree() || this.isMultipleOfFive() || this.isMultipleOfSeven() || this.containsDigitsThreeFiveOrSeven();
+  }
+
+  private isMultipleOfThree() {
+    return this.entry % 3 === 0;
+  }
+
+  private isMultipleOfFive() {
+    return this.entry % 5 === 0;
+  }
+
+  private isMultipleOfSeven() {
+    return this.entry % 7 === 0;
+  }
+
+  private containsDigitsThreeFiveOrSeven() {
+    const specialDigits = ['3', '5', '7', '0'];
+    return specialDigits.some(digit => this.entry.toString().includes(digit));
+  }
+
+  private containsDigitZero() {
+    return this.entry.toString().includes('0');
+  }
+
+  private isNotFooBarQixMultiple() {
+    return !this.isMultipleOfThree() && !this.isMultipleOfFive() && !this.isMultipleOfSeven();
   }
 }
--- test/foobarqix.spec.ts
+++ test/foobarqix.spec.ts
@@ -3,14 +3,30 @@ import {FooBarQix} from "../src/foobarqix";
 
 describe('Foo Bar Qix', () => {
   it.each([
-    [0, '0'],
+    [0, '*'],
     [1, '1'],
     [2, '2'],
-    [3, 'Foo'],
-    //[5, 'Bar'],
-    [6, 'Foo']
-  ])('should return %s when input is %i', (entry, expected) => {
-    const fooBarQix = new FooBarQix();
-    expect(fooBarQix.compute(entry)).toEqual(expected);
+    [3, 'FooFoo'],
+    [4, '4'],
+    [5, 'BarBar'],
+    [6, 'Foo'],
+    [7, 'QixQix'],
+    [8, '8'],
+    [9, 'Foo'],
+    [10, 'Bar*'],
+    [13, 'Foo'],
+    [15, 'FooBarBar'],
+    [21, 'FooQix'],
+    [33, 'FooFooFoo'],
+    [35, 'BarQixFooBar'],
+    [51, 'FooBar'],
+    [53, 'BarFoo'],
+    [101, '1*1'],
+    [105, 'FooBarQix*Bar'],
+    [303, 'FooFoo*Foo'],
+    [10101, 'FooQix**'],
+  ])('For %i, should return %s', (entry, expected) => {
+    const fooBarQix = new FooBarQix(entry);
+    expect(fooBarQix.compute()).toEqual(expected);
   });
 });
